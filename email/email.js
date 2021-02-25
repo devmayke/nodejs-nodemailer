1aaebf8694927d45ab48b6a168081e8fadaaf612
Mayke Ken <devmayke@gmail.com>
2021-02-25 17:28:46 -0300

    refatorado, divisão de responsabilidades

--- email/email.js
+++ email/email.js
@@ -1,16 +1,31 @@
+
 const nodemailer = require('nodemailer')
+
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false, 
     auth:{
         user: "devmayke@gmail.com",
-        pass: ""
+        pass: "Codebase1@1"
     }
-});
-transporter.sendMail({
-    from:
 })
+function enviaEmail(req, res){
+    transporter.sendMail({
+        from: `${req.body.nome} <devmayke@gmail.com>`,
+        to: "incognator@gmail.com",
+        subject:`Formuário criado no servido node`,
+        html:`Enviado de: ${req.body.email}`+`<br>`+`${req.body.mensagem}`
+    }).then((sucesso)=>{
+        console.log(sucesso)
+        res.send('email enviado com sucesso')
+    }).catch((err)=>{
+    console.log(err)}) 
 
+}
 
-module.exports = nodemailer;
+module.exports = {
+    nodemailer, 
+    transporter,
+    enviaEmail
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,33 +1,13 @@
+const nodemailer = require( '../email/email.js')
 const express = require('express');
-const { renameSync } = require('fs');
 const app = express();
 const router = express.Router();
-
-const nodemailer = require('nodemailer')
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, 
-    auth:{
-        user: "devmayke@gmail.com",
-        pass: ""
-    }
-});
+const transporter = nodemailer.transporter;
+const enviaEmail = nodemailer.enviaEmail;
 
 
 router.get("/formulario", (req, res)=>{
     res.render('form')
 })
-router.post("/formulario", (req, res)=>{
-    transporter.sendMail({
-        from: `${req.body.nome} <devmayke@gmail.com>`,
-        to: "incognator@gmail.com",
-        subject:`Formuário criado no servido node`,
-        html:`Enviado de: ${req.body.email}`+`<br>`+`${req.body.mensagem}`
-    }).then((sucesso)=>{
-        console.log(sucesso)
-        res.send('email enviado com sucesso')
-    }).catch((err)=>
-    console.log(err))    
-})
+router.post("/formulario", (req, res)=>enviaEmail(req, res))
 module.exports = router;
