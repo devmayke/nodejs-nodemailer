3986c1ab346eaced69edac076d1bdc52d3b132f5
Mayke Ken <devmayke@gmail.com>
2021-02-27 18:27:58 -0300

    rota criada e linkada com ahref no handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const admin = require('./routes');
+
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser')
 const PORT = 3000
@@ -16,8 +17,6 @@ app.set("view engine", "handlebars")
 
 app.use('/', admin)
 
-app.listen(PORT, (err)=>{
-    console.log("Servidor iniciado em http://127.0.0.1:"+PORT+"/formulario")
+app.listen(PORT, ()=>{
+     console.log("Servidor iniciado em http://127.0.0.1:"+PORT+"/formulario");  
 })
-
-
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const nodemailer = require('./email.js')
 const formulario = require('./formulario')
 
+
+
+router.get("/teste", function(req, res ){
+    res.render('teste')
+
+})
+
 router.get("/formulario", formulario.get)
 
 router.post("/formulario", nodemailer.post)
