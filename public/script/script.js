44fa523802fc1de7aef39ada7155e6ed217e51a6
Mayke Ken <devmayke@gmail.com>
2021-03-02 16:44:14 -0300

    css

--- public/script/script.js
+++ public/script/script.js
@@ -8,3 +8,11 @@ function getLocalStorage(dados){
     return JSON.parse(localStorage.getItem(dados))
 }
 console.log(getLocalStorage(dados))
+
+const numberOfLetters = document.querySelector('textarea');
+const counterIntarface = document.querySelector('span');
+const limit= 200;
+numberOfLetters.addEventListener("keyup",(e)=>{     
+    console.log(numberOfLetters.value) 
+    counterIntarface.innerText=numberOfLetters.value.length==0?limit:limit - numberOfLetters.value.length;    
+})
